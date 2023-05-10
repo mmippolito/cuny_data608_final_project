35363cfee54c725d840e66810ec46166f419c58c
Michael Ippolito <michael.ippolito@gmail.com>
2023-05-10 06:59:39 -0500

    '.'

--- OLD2_ippolito_project.py
+++ OLD2_ippolito_project.py
@@ -67,7 +67,10 @@ def populate_dd(cat, subcat):
 	return d
 
 # General function to update graph, called from callback function
-def update_gr_general(outcome_var, indep_var, chk, yr):
+def update_gr_general(outcome_var, indep_var, chk):
+
+	# Copy chk_options
+	#chk_options2 = chk_options.copy()
 
 	# Check for error
 	if outcome_var is None or indep_var is None:
@@ -101,17 +104,14 @@ def update_gr_general(outcome_var, indep_var, chk, yr):
 	fig1.update_layout(title='', plot_bgcolor='white')
 
 	# Display map
-	dfplot1 = dfplot1[dfplot1['year'] == yr]
+	dfplot1 = dfplot1[dfplot1['year'] == 2002]
 	if dfvar[dfvar['var'] == indep_var]['vartype'].values[0] == 'cat':
-		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=indep_var, \
-			color_discrete_map={'no': 'cornsilk', 'yes': 'cadetblue', 'na': 'whitesmoke'}, width=500)
+		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=indep_var, color_discrete_sequence=['cadetblue', 'cornsilk'], width=500)
 	else:
-		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=indep_var, \
-			color_continuous_scale='deep', width=500)
+		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=indep_var, color_continuous_scale='deep', width=500)
 	fig2.update_layout(
 		plot_bgcolor='white',
-		title={'text': dfvar[dfvar['var'] == indep_var]['descr'].values[0] + ' - ' + str(yr), 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, \
-			'font': {'family': 'Calibri', 'size': 14}}
+		title={'text': dfvar[dfvar['var'] == indep_var]['descr'].values[0] + ' - 2002', 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, 'font': {'family': 'Calibri', 'size': 14}}
 	)
 
 	# Return plot
@@ -198,7 +198,7 @@ app.layout = html.Div(
 			], style={'border': 'none'}),
 
 			############################
-			# State maps - TISS
+			# State map - TISS
 			############################
 			html.Tr([
 				html.Td([
@@ -207,25 +207,23 @@ app.layout = html.Div(
 			], style={'border': 'none', 'padding': '0px'}),
 			html.Tr([
 				html.Td([
-					dcc.Graph(id='gr_map1', style={'width': '500px'})
-				], style={'border': 'none', 'padding': '0px'}),
+					dcc.Graph(id='gr_map1', style={'width': '600px'})
+				], style={'border': 'none', 'padding': '0px'}, colSpan=1),
 				html.Td([
-					dcc.Graph(id='gr_map2', style={'width': '500px'})
-				], style={'border': 'none', 'padding': '0px'})
+					dcc.Graph(id='gr_map2', style={'width': '600px'})
+				], style={'border': 'none', 'padding': '0px'}, colSpan=1)
 			], style={'border': 'none', 'padding': '1px'}),
 			html.Tr([
 				html.Td([
-					dcc.Graph(id='gr_map3', style={'width': '500px'})
-				], style={'font-size': '14px', 'border': 'none', 'padding': '1px'}),
+					dcc.Graph(id='gr_map3', style={'width': '600px'})
+				], style={'font-size': '14px', 'border': 'none', 'padding': '1px'}, colSpan=1),
 			], style={'border': 'none', 'padding': '0px'}),
 			html.Tr([
 				html.Td([
-					html.Div([
-						dcc.Slider(id='sl_year', min=1975, max=2002, step=3, value=1975, marks=d_yr, updatemode='drag'),
-						html.Button(id='but_play', children='Play', n_clicks=0, disabled=False),
-						dcc.Interval(id="ani_map", disabled=True, interval=1000, n_intervals=0, max_intervals=-1)
-					], style={'width': '75%', 'margin': 'auto'})
-				], style={'border': 'none', 'padding': '1px', 'text-align': 'left'})
+					dcc.Slider(id='sl_year', min=1975, max=2002, step=3, value=1975, marks=d_yr, updatemode='drag')
+					#html.Button(id='but_play', value='Play', n_clicks=0, disabled=False),
+					#dcc.Interval(id="ani_map", disabled=True)
+				], style={'border': 'none', 'padding': '1px', 'text-align': 'left'}, colSpan=1)
 			], style={'border': 'none', 'padding': '1px'}),
 			html.Tr([
 				html.Td([
@@ -335,18 +333,6 @@ app.layout = html.Div(
 						], style={'border': 'none', 'padding': '0px'}),  #tr
 						html.Tr([
 							html.Td([
-								html.Label([''])
-							], style={'font-size': '14px', 'border': 'none', 'padding': '1px', 'height': '40px'}, colSpan=2),
-							html.Td([
-								html.Div([
-									dcc.Slider(id='sl_year2', min=1975, max=2002, step=3, value=1975, marks=d_yr, updatemode='drag'),
-									html.Button(id='but_play2', children='Play', n_clicks=0, disabled=False),
-									dcc.Interval(id="ani_map2", disabled=True, interval=1000, n_intervals=0, max_intervals=-1)
-								], style={'width': '75%', 'margin': 'auto'})
-							], style={'font-size': '14px', 'border': 'none', 'padding': '1px', 'height': '40px'}, colSpan=2)
-						], style={'border': 'none', 'padding': '0px'}),
-						html.Tr([
-							html.Td([
 								html.Hr()
 							], style={'font-size': '14px', 'border': 'none', 'padding': '1px', 'height': '40px'}, colSpan=4)
 						], style={'border': 'none', 'padding': '0px'}),
@@ -413,18 +399,6 @@ app.layout = html.Div(
 						], style={'border': 'none', 'padding': '0px'}),  #tr
 						html.Tr([
 							html.Td([
-								html.Label([''])
-							], style={'font-size': '14px', 'border': 'none', 'padding': '1px', 'height': '40px'}, colSpan=2),
-							html.Td([
-								html.Div([
-									dcc.Slider(id='sl_year3', min=1975, max=2002, step=3, value=1975, marks=d_yr, updatemode='drag'),
-									html.Button(id='but_play3', children='Play', n_clicks=0, disabled=False),
-									dcc.Interval(id="ani_map3", disabled=True, interval=1000, n_intervals=0, max_intervals=-1)
-								], style={'width': '75%', 'margin': 'auto'})
-							], style={'font-size': '14px', 'border': 'none', 'padding': '1px', 'height': '40px'}, colSpan=2)
-						], style={'border': 'none', 'padding': '0px'}),
-						html.Tr([
-							html.Td([
 								html.P("As seen above, the predicted incarceration rate without TIS laws is significantly lower than the actual rate. This seems to counter the prediction that prison admissions would have been higher absent TIS laws, but it is likely that this is simply a reflection of the growing overall population. Overall crime rates seem unaffected by TIS, while violent crime rate was predicted to have been higher without TIS. Since TIS mostly applies to violent offenses, this would seem to indicate it is at least partially working.", 
 									style={'font-size': '14px', 'text-align': 'left'}
 								),
@@ -459,57 +433,110 @@ app.layout = html.Div(
 					], style={'border-collapse': 'collapse', 'padding': '0px', 'border': 'none'})
 				], style={'border': 'none', 'padding': '0px'}, colSpan=2)
 			], style={'border': 'none', 'padding': '0px'}),
-		], style={'border-collapse': 'collapse', 'padding': '0px', 'border': 'none'})
+		], style={'border-collapse': 'collapse', 'padding': '0px', 'border': 'none'}),
+		html.Div(id='dummy_div')  # dummy div to signal app to load data
 	], style={'margin': 'auto', 'width': '60%', 'padding': '10px'}
 )
 
 #-----------------------------------------------------
 
-# Callback functions to update tis maps
+# Callback functions to update map
 @app.callback(
 	Output('gr_map1', 'figure'),
 	Output('gr_map2', 'figure'),
 	Output('gr_map3', 'figure'),
+	Input('dummy_div', 'id'),
 	Input('sl_year', 'value'),
 	prevent_initial_call=False
 )
-def update_tis_maps(sl_year):
+def update_map1(dummy_div, sl_year):
 
 	# Fetch data
 	dfplot = df1[df1['year'] == sl_year]
+	#dfplot['tmp_agg'] = np.where((dfplot['pre_par']=='yes') | (dfplot['presum_s']=='yes'), 'yes', 'no')
 
 	# Display map1 - tiss
-	fig1 = px.choropleth(dfplot, locations='state_ab', locationmode='USA-states', scope='usa', color='tiss', \
-		color_discrete_map={'no': 'cornsilk', 'yes': 'cadetblue', 'na': 'whitesmoke'}, width=500)
+	fig1 = px.choropleth(dfplot, locations='state_ab', locationmode='USA-states', scope='usa', color='tiss', color_discrete_map={'no': 'cornsilk', 'yes': 'cadetblue'}, width=500)
 	fig1.update_layout(
 		plot_bgcolor='white',
-		title={'text': 'Truth-in-Sentencing Policy - ' + str(sl_year), 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, \
-			'font': {'family': 'Calibri', 'size': 18}}
+		title={'text': 'Truth-in-Sentencing Policy - ' + str(sl_year), 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, 'font': {'family': 'Calibri', 'size': 18}}
 	)
 
 	# Display map2 - aggregated presumptive guidelines
-	fig2 = px.choropleth(dfplot, locations='state_ab', locationmode='USA-states', scope='usa', color='tisviol_agg', \
-		color_discrete_map={'no': 'cornsilk', 'yes': 'cadetblue', 'na': 'whitesmoke'}, width=500)
+	fig2 = px.choropleth(dfplot, locations='state_ab', locationmode='USA-states', scope='usa', color='tisviol_agg', color_discrete_map={'no': 'cornsilk', 'yes': 'cadetblue'}, width=500)
 	fig2.update_layout(
 		plot_bgcolor='white',
-		title={'text': 'Mandatory sentencing for violent crime - ' + str(sl_year), 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, \
-			'font': {'family': 'Calibri', 'size': 18}}
+		title={'text': 'Mandatory sentencing for violent crime - ' + str(sl_year), 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, 'font': {'family': 'Calibri', 'size': 18}}
 	)
 
-	# Display mean tisp by presence of determinate sentencing
+	# Display map3 - mean tisp
+	#dfplot_mean = dfplot[dfplot['tiss'] == 'yes'].groupby(['year'], as_index=False).agg({'tisp': 'mean'})
+	##dfplot_mean = dfplot.groupby(['year'], as_index=False).apply(lambda x: np.average(x.tisp, weights=x.pop_l1))
+	#dfplot_mean.columns.values[1] = 'tisp'
+	#fig3 = px.bar(dfplot_mean, orientation='h', x='tisp', y='pre_par', height=120, width=600)
+	#fig3.update_layout(
+	#	plot_bgcolor='white',
+	#	title={'text': 'Mean percentage of sentence offenders required to serve - ' + str(sl_year), 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9},
+	#	yaxis={'visible': False, 'showticklabels': False},
+	#	xaxis={'title': '', 'range': [0, 100]}
+	#)
+
+	# Display map3 - mean tisp by presence of determinate sentencing
 	dfplot_mean = dfplot.groupby(['pre_par'], as_index=False).agg({'tisp': 'mean'})
 	dfplot_mean.columns.values[1] = 'tisp'
-	fig3 = px.bar(dfplot_mean, orientation='h', x='tisp', y='pre_par', height=180, width=500)
+	fig3 = px.bar(dfplot_mean, orientation='h', x='tisp', y='pre_par', height=180, width=600)
 	fig3.update_layout(
 		plot_bgcolor='white',
-		title={'text': 'Mean % of sentence required (by determinate sentencing) - ' + str(sl_year) , 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, \
-			'font': {'family': 'Calibri', 'size': 16}},
+		title={'text': 'Mean % of sentence required (by determinate sentencing) - ' + str(sl_year) , 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, 'font': {'family': 'Calibri', 'size': 18}},
 		yaxis={'title': '', 'visible': True, 'showticklabels': True},
 		xaxis={'title': '', 'range': [0, 100]}
 	)
 
 	# Return
 	return fig1, fig2, fig3
+
+"""
+# Callback functions for play/stop button
+@app.callback(
+	Output('ani_map', 'disabled'),
+	Output('but_play', 'value'),
+	Input('but_play', 'n_clicks'),
+	Input('ani_map', 'disabled'),
+	prevent_initial_call=True
+)
+def enable_animate(but_play, ani_map):
+
+	# Update interval object
+	print('here1', ani_map, but_play)
+	if but_play % 1 == 1:
+		print('here2', ani_map, but_play)
+		return False, 'Stop'
+	else:
+		print('here5', ani_map, but_play)
+		return True, 'Play'
+	print('here6', ani_map, but_play)
+	return False, 'Stop'
+
+# Callback functions to animate map
+@app.callback(
+	Output('sl_year', 'value'),
+	Input('sl_year', 'value'),
+	Input('ani_map', 'disabled'),
+	prevent_initial_call=False
+)
+def animate_map1(sl_year, ani_map):
+
+	# Update slider year
+	print('here3', ani_map)
+	newyr = sl_year
+	if not ani_map:
+		print('here4', ani_map)
+		newyr += 3
+		if newyr > 2002: newyr = 1975
+
+	# Return
+	return newyr
+"""
 
 #-----------------------------------------------------
 
@@ -535,8 +562,6 @@ def update_dyn_options(dd_cat):
 	except:
 		return '', {}, '', '', ''
 
-#-----------------------------------------------------
-
 # Callback functions to update dynamic graphs
 @app.callback(
 	Output('gr_dyn', 'figure'),
@@ -545,17 +570,14 @@ def update_dyn_options(dd_cat):
 	Input('dd_outcome_dyn', 'value'),
 	Input('dd_indep_dyn', 'value'),
 	Input('chk_dyn', 'value'),
-	Input('sl_year2', 'value'),
 	prevent_initial_call=False
 )
-def update_dyn_gr(outcome_var, indep_var, chk, yr):
+def update_dyn_gr(outcome_var, indep_var, chk):
 	try:
-		r1, r2, chk = update_gr_general(outcome_var, indep_var, chk, yr)
+		r1, r2, chk = update_gr_general(outcome_var, indep_var, chk)
 		return r1, r2, chk
 	except:
 		return {}, {}, {}
-
-#-----------------------------------------------------
 
 # Callback functions to update tis effect graphs
 @app.callback(
@@ -563,10 +585,9 @@ def update_dyn_gr(outcome_var, indep_var, chk, yr):
 	Output('map_tiseff', 'figure'),
 	Input('dd_outcome_tiseff', 'value'),
 	Input('chk_mean', 'value'),
-	Input('sl_year3', 'value'),
 	prevent_initial_call=False
 )
-def update_gr_tiseff(outcome_var, chk_mean, mapyr):
+def update_gr_tiseff(outcome_var, chk_mean):
 
 	if outcome_var is None:
 		print('returning empty dict')
@@ -593,17 +614,16 @@ def update_gr_tiseff(outcome_var, chk_mean, mapyr):
 	fig1.update_layout(title='', plot_bgcolor='white')
 
 	# Choose year for map; some vars are NaN for 2002
-	#if outcome_var == 'adm_100k' or outcome_var == 'ncourtc' or outcome_var == 'court_10':
-	#	mapyr = 1996
+	mapyr = 2002
+	if outcome_var == 'adm_100k' or outcome_var == 'ncourtc' or outcome_var == 'court_10':
+		mapyr = 1996
 	dfplot1 = dfplot1[dfplot1['year'] == mapyr]
 
 	# Map
 	if dfvar[dfvar['var'] == outcome_var]['vartype'].values[0] == 'cat':
-		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=outcome_var, \
-			color_discrete_map={'no': 'cornsilk', 'yes': 'cadetblue', 'na': 'whitesmoke'}, width=500)
+		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=outcome_var, color_discrete_sequence=['cadetblue', 'cornsilk'], width=500)
 	else:
-		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=outcome_var, \
-			color_continuous_scale='deep', width=500)
+		fig2 = px.choropleth(dfplot1, locations='state_ab', locationmode='USA-states', scope='usa', color=outcome_var, color_continuous_scale='deep', width=500)
 	fig2.update_layout(
 		plot_bgcolor='white',
 		title={'text': dfvar[dfvar['var'] == outcome_var]['descr'].values[0] + ' - ' + str(mapyr), 'xanchor': 'center', 'yanchor': 'top', 'x': 0.5, 'y': 0.9, 'font': {'family': 'Calibri', 'size': 18}}
@@ -611,99 +631,6 @@ def update_gr_tiseff(outcome_var, chk_mean, mapyr):
 
 	# Return plot
 	return fig1, fig2
-
-#-----------------------------------------------------
-
-# Callback functions for play/stop button
-@app.callback(
-	Output('but_play', 'children'),
-	Output('ani_map', 'disabled'),
-	Input('but_play', 'n_clicks'),
-	prevent_initial_call=True
-)
-def enable_animate(but_play):
-	if but_play % 2 == 0:
-		return 'Play', True
-	else:
-		return 'Stop', False
-
-#-----------------------------------------------------
-
-# Callback functions to animate map
-@app.callback(
-	Output('sl_year', 'value'),
-	Input('sl_year', 'value'),
-	Input('ani_map', 'disabled'),
-	Input('ani_map', 'n_intervals')
-)
-def animate_map(sl_year, ani_map, n_int):
-	newyr = sl_year
-	if not ani_map:
-		newyr += 3
-		if newyr > 2002: newyr = 1975
-	return newyr
-
-#-----------------------------------------------------
-
-# Callback functions for play/stop button (dynamic maps)
-@app.callback(
-	Output('but_play2', 'children'),
-	Output('ani_map2', 'disabled'),
-	Input('but_play2', 'n_clicks'),
-	prevent_initial_call=True
-)
-def enable_animate_dyn(but_play):
-	if but_play % 2 == 0:
-		return 'Play', True
-	else:
-		return 'Stop', False
-
-#-----------------------------------------------------
-
-# Callback functions to animate map (dynamic maps)
-@app.callback(
-	Output('sl_year2', 'value'),
-	Input('sl_year2', 'value'),
-	Input('ani_map2', 'disabled'),
-	Input('ani_map2', 'n_intervals')
-)
-def animate_map_dyn(sl_year, ani_map, n_int):
-	newyr = sl_year
-	if not ani_map:
-		newyr += 3
-		if newyr > 2002: newyr = 1975
-	return newyr
-
-#-----------------------------------------------------
-
-# Callback functions for play/stop button (tis effects)
-@app.callback(
-	Output('but_play3', 'children'),
-	Output('ani_map3', 'disabled'),
-	Input('but_play3', 'n_clicks'),
-	prevent_initial_call=True
-)
-def enable_animate_tiseff(but_play):
-	if but_play % 2 == 0:
-		return 'Play', True
-	else:
-		return 'Stop', False
-
-#-----------------------------------------------------
-
-# Callback functions to animate map (tis effects)
-@app.callback(
-	Output('sl_year3', 'value'),
-	Input('sl_year3', 'value'),
-	Input('ani_map3', 'disabled'),
-	Input('ani_map3', 'n_intervals')
-)
-def animate_map_tiseff(sl_year, ani_map, n_int):
-	newyr = sl_year
-	if not ani_map:
-		newyr += 3
-		if newyr > 2002: newyr = 1975
-	return newyr
 
 #-----------------------------------------------------
 
